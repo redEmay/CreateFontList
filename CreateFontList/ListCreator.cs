e87fc537db8656f732679fa4be36bb87ca5fe589
Erik May <emay@cpscards.com>
2021-06-25 17:01:31 -0400

    Added in functionality to take a screenshot of windows.

--- CreateFontList/ListCreator.cs
+++ CreateFontList/ListCreator.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace CreateFontList
 {
     public class ListCreator
     {
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int SW_MAXIMIZE = 3;
+
         public static void CreateList()
         {
             var appSettings = new AppSettings();
@@ -68,7 +76,10 @@ namespace CreateFontList
             {
                 foreach (var font in fontList)
                 {
-                    sw.WriteLine($"{font.Item1.Replace("C:/temp/FontFolder", "G:/Apps/Fonts").Trim()},{font.Item2.Trim()}");
+                    if (font.Item2.Length > 50)
+                        sw.WriteLine($"{font.Item1.Replace("C:/temp/FontFolder", "G:/Apps/Fonts").Trim()},{font.Item2.Trim().Substring(0, 50)}");
+                    else
+                        sw.WriteLine($"{font.Item1.Replace("C:/temp/FontFolder", "G:/Apps/Fonts").Trim()},{font.Item2.Trim()}");
                 }
             }
 
@@ -83,7 +94,11 @@ namespace CreateFontList
             ProcessStartInfo objProcess = new ProcessStartInfo();
             objProcess.UseShellExecute = true;
             objProcess.RedirectStandardOutput = false;
+
+            var rect = new User32.Rect();
+
             var i = 0;
+
             for (int j = 0; j < directoryList.Count; j++)
             {
                 i++;
@@ -95,7 +110,20 @@ namespace CreateFontList
 
                 var process = Process.GetProcesses().Where(p => p.MainWindowTitle != string.Empty).
                     Except(initialProcessList.Where(p => p.MainWindowTitle != string.Empty)).
-                    OrderBy(p => p.StartTime).Last();
+                    OrderBy(p => p.StartTime).Last();   //get process 
+
+                ShowWindow(process.MainWindowHandle, SW_MAXIMIZE);  //maximizes window
+                Thread.Sleep(1000);
+
+                User32.GetWindowRect(process.MainWindowHandle, ref rect);   //This will change the values in rect object
+
+                var bmp = new Bitmap(rect.right - rect.left, rect.bottom - rect.top, PixelFormat.Format32bppArgb);
+                using (Graphics graphics = Graphics.FromImage(bmp))
+                {
+                    graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(rect.right - rect.left, rect.bottom - rect.top), CopyPixelOperation.SourceCopy);
+                    //graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(1000, 1000), CopyPixelOperation.SourceCopy);
+                    //graphics.CopyFromScreen(rect.left, rect.top, 200, 200, new Size(rect.right - rect.left, rect.bottom - rect.top), CopyPixelOperation.SourceCopy);
+                }
 
                 if (process.MainWindowTitle.Contains("(TrueType)"))
                 {
@@ -103,25 +131,54 @@ namespace CreateFontList
                         Console.WriteLine();
 
                     font.Add(Tuple.Create(directoryList[j], process.MainWindowTitle.Replace("(TrueType)", "")));
-                    listResult.Add(process.MainWindowTitle.Replace("(TrueType)", ""));                    
+                    listResult.Add(process.MainWindowTitle.Replace("(TrueType)", ""));
+
+                    if(process.MainWindowTitle.Length>20)
+                        bmp.Save($@"C:\Users\Erik May\Documents\Images\{process.MainWindowTitle.Replace("(TrueType)", "").Substring(0, 10)}[{j}].jpeg",
+                            ImageFormat.Jpeg);
+                    else
+                        bmp.Save($@"C:\Users\Erik May\Documents\Images\{process.MainWindowTitle.Replace("(TrueType)", "")}[{j}].jpeg",
+                            ImageFormat.Jpeg);
                 }
                 else if (process.MainWindowTitle.Contains("(OpenType)"))
                 {
                     font.Add(Tuple.Create(directoryList[j], process.MainWindowTitle.Replace("(OpenType)", "")));
                     listResult.Add(process.MainWindowTitle.Replace("(OpenType)", ""));
+
+                    if(process.MainWindowTitle.Length>20)
+                        bmp.Save($@"C:\Users\Erik May\Documents\Images\{process.MainWindowTitle.Replace("(OpenType)", "").Substring(0, 10)}.jpeg",
+                            ImageFormat.Jpeg);
+                    else
+                        bmp.Save($@"C:\Users\Erik May\Documents\Images\{process.MainWindowTitle.Replace("(OpenType)", "")}.jpeg",
+                           ImageFormat.Jpeg);
                 }
                 else
                 {
                     font.Add(Tuple.Create($"{directoryList[j].Replace(targetDirectory, @"G:\Apps\Fonts\")}", process.MainWindowTitle));
                     listResult.Add(process.MainWindowTitle);
+
+                    bmp.Save($@"C:\Users\Erik May\Documents\Images\{process.MainWindowTitle}.jpeg", ImageFormat.Jpeg);
                 }
 
                 process.Kill();
-
-                Thread.Sleep(500);
             }
             var result = listResult.Distinct();
             return result;
         }
+    }
+
+    public static class User32
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
     }
 }
